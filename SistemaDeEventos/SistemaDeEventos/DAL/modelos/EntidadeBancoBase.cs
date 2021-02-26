07a4dc52606af0d2591f53fb825e45e16c8bf5fb
Daniela Luiza Duwe Heiderscheidt - DDW <daniela.duwe@iteris.com.br>
2021-02-26 14:09:07 -0300

    modelos file and evento repositorio

---
+++ SistemaDeEventos/SistemaDeEventos/DAL/modelos/EntidadeBancoBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeEventos.DAL
+{
+	public abstract class EntidadeBancoBase
+	{
+		[Required]
+		[StringLength(255)]
+		public string UsuarioCriacao { get; set; }
+		[Required]
+		public DateTime DataCriacao { get; set; }
+
+		[Required]
+		public bool FlagExcluido { get; set; }
+	}
+}
