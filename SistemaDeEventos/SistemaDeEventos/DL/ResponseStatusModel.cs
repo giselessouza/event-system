e4db36ed45a17d17579ba340f377abedd2692f43
Daniela Luiza Duwe Heiderscheidt - DDW <daniela.duwe@iteris.com.br>
2021-03-02 12:26:09 -0300

    Status CRUD

---
+++ SistemaDeEventos/SistemaDeEventos/DL/ResponseStatusModel.cs
@@ -0,0 +1,17 @@
+﻿using SistemaDeEventos.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeEventos.DL
+{
+    public class ResponseStatusModel : UpdateStatusModel
+    {
+        public ResponseStatusModel(StatusEvento modeldb)
+        {
+            this.idEvento = modeldb.IdEventoStatus;
+            this.NomeStatus = modeldb.NomeStatus;
+        }
+    }
+}
