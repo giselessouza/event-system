1702e14c1e4b1bff036119a03eff8cc69793ee71
Gisele de Souza Santos - GSZ <gisele.souza@iteris.com.br>
2021-03-02 14:16:31 -0300

    classes de categoria

---
+++ SistemaDeEventos/SistemaDeEventos/DAL/CategoriaRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeEventos.DAL
+{
+    public class CategoriaRepositorio
+    {
+        public List<CategoriaEvento> Listar()
+        {
+            using (var db = new SistemaDeEventosContext())
+            {
+
+                return db.CategoriaEventos.ToList();
+            }
+        }
+
+    }
+}
---
+++ SistemaDeEventos/SistemaDeEventos/DL/CreateCategoriaModel.cs
@@ -0,0 +1,16 @@
+﻿using SistemaDeEventos.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeEventos.DL
+{
+    public class CreateCategoriaModel
+    {
+        public int IdCategoriaEvento { get; set; }
+        public string NomeCategoria { get; set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+    }
+}
